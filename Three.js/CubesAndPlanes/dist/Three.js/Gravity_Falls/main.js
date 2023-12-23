c0e386685d270cbdf1e21c36e27e6fc6788ad77c
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-12-23 20:34:51 +0100

    App

--- Three.js/CubesAndPlanes/dist/Three.js/Gravity_Falls/main.js
+++ Three.js/CubesAndPlanes/dist/Three.js/Gravity_Falls/main.js
@@ -1,16 +1,16 @@
 // FOR ONLINE IMPLEMENTATION
-/*
+
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.125.2/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
-*/
+
 
 // FOR LOCAL IMPLEMENTATION
-
+/*
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
-
+*/
 
 // 1. RENDERER:
 const renderer = new THREE.WebGLRenderer({antialias: true});
