fd5ebf23c704c2ec4cf0720cda8e3e0273ee920d
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-12-23 20:35:04 +0100

    Updates

--- Three.js/Gravity_Falls/main.js
+++ Three.js/Gravity_Falls/main.js
@@ -1,7 +1,9 @@
 // FOR ONLINE IMPLEMENTATION
+
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.125.2/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+
 
 // FOR LOCAL IMPLEMENTATION
 /*
@@ -25,26 +27,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     45, window.innerWidth/ window.innerHeight, 1, 1000
 );
-camera.position.set(4, 5, 11);
-camera.lookAt(0, 0, 0);
+camera.position.set(4, 5, 20);
 
-// OTHER:
+// OBJECTS:
 
-// a) OBJECTS:
-const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-groundGeometry.rotateX(-Math.PI/2);
-const groundMaterial = new THREE.MeshStandardMaterial({
-    color:0x555555,
-    side:THREE.DoubleSide
-})
-const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial)
-groundMesh.castShadow = false;
-groundMesh.receiveShadow = true;
-scene.add(groundMesh);
-
-// 3D MODEL:
+// A) 3D MODEL:
 const loader = new GLTFLoader().setPath("./public/");
-loader.load('scene.gltf', (gltf) => {
+loader.load('GravityFalls.gltf', (gltf) => {
     const mesh = gltf.scene;
   
     mesh.traverse((child) => {
@@ -53,35 +42,39 @@ loader.load('scene.gltf', (gltf) => {
         child.receiveShadow = true;
       }
     });
-  
-    mesh.position.set(0, 1.05, -1);
+    
+    mesh.position.set(0, -3.00, -0.5);
     scene.add(mesh);
   
     document.getElementById('progress-container').style.display = 'none';
   }, ( xhr ) => {
     document.getElementById('progress').innerHTML = `LOADING ${Math.max(xhr.loaded / xhr.total, 1) * 100}/100`;
-  },);
+},);
 
 // Controls:
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
-controls.minDistance = 5;
+controls.minDistance = 10;
 controls.maxDistance = 20;
 controls.minPolarAngle = 0.5;
-controls.maxPolarAngle = 1.5;
-controls.autoRotate = false;
+controls.maxPolarAngle = 5.5;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
 // b) LIGHTS:
 const spotLight = new THREE.SpotLight(
-    0xCCAAFF,  5, 100, 0.22, 0.5
+    0xCCAAFF,  5, 0, 0.22, 0.5
 );
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow=true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
+
+const ambientLight = new THREE.AmbientLight(
+  0xFFFFFF, 2.1
+)
+scene.add(ambientLight)
 
 // c) SHADOWS:
 renderer.shadowMap.enabled = true;
@@ -91,6 +84,34 @@ var max = Math.random()
 var current = 0.0;
 var min = 0
 var go = true
+
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// AUDIO
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+var sound = new THREE.Audio( listener );
+
+var audioLoader = new THREE.AudioLoader();
+
+//Load a sound and set it as the Audio object's buffer
+audioLoader.load( './music.mp3', function( buffer ) {
+    sound.setBuffer( buffer );
+    sound.setLoop(true);
+    sound.setVolume(0.5);
+    sound.play();
+},
+function ( xhr ) {
+    console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+},
+
+function ( err ) {
+    console.log( 'Un error ha ocurrido' );
+}
+);
 
 // ANIMATION:
 function animate(){
