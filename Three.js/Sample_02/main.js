ae046c6863fd6cbe966edbb0f574115e4425b733
Juliusz Losinski <72278818+juliuszlosinski@users.noreply.github.com>
2023-12-23 18:10:15 +0100

    Updates

--- Three.js/Sample_02/main.js
+++ Three.js/Sample_02/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.125.2/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.125.2/build/three.module.js'
 
 // PARAMETRY
 var windowWidth = window.innerWidth   // Szerokość okna.
